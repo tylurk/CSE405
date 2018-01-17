02aa5384e98da0527cd8d0e3b448e30d73580301
Tyler Clinscales <34224275+tylurk@users.noreply.github.com>
2018-01-17 14:49:03 -0800

    Update client.js

--- http/client.js
+++ http/client.js
@@ -4,3 +4,15 @@ const socket = net.createConnection({
   host: 'localhost',
   port: 8000
 });
+
+socket.on('connect', () => {
+  const req =
+    'GET / HTTP/1.1  \r\n' +
+    'Host: localhost \r\n' +
+    '\r\n';
+  socket.end(req);  // Send req string and then close this side of the TCP socket.
+});
+
+socket.on('data', (chunk) => {
+  console.log(chunk.toString('data event occured'));
+});
